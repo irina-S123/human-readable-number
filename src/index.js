9a6f96666016eb0708a0bda90a4d2d81291dfab7
irina-S123 <123304775+irina-S123@users.noreply.github.com>
2025-09-07 20:32:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
-function toReadable(number) {
-  const ones = [
-    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-    'seventeen', 'eighteen', 'nineteen'
-  ];
+function numberToReadableString(num) {
+  const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+  const thousands = ["", "thousand", "million", "billion"];
 
-  const tens = [
-    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
-  ];
+  if (num === 0) return "zero";
 
-  if (number === 0) return 'zero';
+  let words = [];
+  let i = 0;
 
-  function helper(n) {
-    let result = '';
+  while (num > 0) {
+    let chunk = num % 1000;
+    if (chunk) {
+      let chunkWords = [];
+      let hundreds = Math.floor(chunk / 100);
+      let remainder = chunk % 100;
 
-    if (n < 20) {
-      result = ones[n];
-    } else if (n < 100) {
-      result = tens[Math.floor(n / 10)];
-      if (n % 10) result += ' ' + ones[n % 10];
-    } else if (n < 1000) {
-      result = ones[Math.floor(n / 100)] + ' hundred';
-      if (n % 100) result += ' ' + helper(n % 100);
+      if (hundreds) {
+        chunkWords.push(ones[hundreds], "hundred");
+      }
+
+      if (remainder) {
+        if (remainder < 10) {
+          chunkWords.push(ones[remainder]);
+        } else if (remainder > 10 && remainder < 20) {
+          chunkWords.push(teens[remainder - 10]);
+        } else {
+          chunkWords.push(tens[Math.floor(remainder / 10)]);
+          if (remainder % 10) {
+            chunkWords.push(ones[remainder % 10]);
+          }
+        }
+      }
+
+      if (thousands[i]) {
+        chunkWords.push(thousands[i]);
+      }
+
+      words.unshift(chunkWords.join(" "));
     }
-    return result;
+    num = Math.floor(num / 1000);
+    i++;
   }
 
-  return helper(number);
+  return words.join(" ");
 }
-console.log(toReadable(number));
+
+// Example usage:
+console.log(numberToReadableString(123456789));
